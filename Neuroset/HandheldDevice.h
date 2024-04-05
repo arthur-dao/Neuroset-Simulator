c89ca708c4b5fbd32f89c031b304cfde15033003
HughHang <hughhang30@gmail.com>
2024-04-04 22:41:52 -0400

    Added project files

---
+++ Neuroset/HandheldDevice.h
@@ -0,0 +1,44 @@
+#ifndef HANDHELDDEVICE_H
+#define HANDHELDDEVICE_H
+
+#include "defs.h"
+#include "Headset.h"
+#include "Session.h"
+
+#include "QTimer"
+
+class HandheldDevice{
+
+    public:
+        HandheldDevice();
+        void createSession();
+        void pauseSession();
+        void shutdown();
+        void beginSession();
+        void dateTimeSelection();
+        void displaySessionLog();
+
+        void stop();
+        void pause();
+        void resume();
+
+        void navigate();
+        void select();
+        void menuToggle();
+        void uploadToPC();
+        void updateMenu();
+
+        void powerToggle();
+
+    private:
+        RunStatus status;
+        int batteryPercent;
+
+        QTimer  stopTimer;
+        QTimer  runTimer;
+
+        list<Session> sessions;
+        static int size;
+};
+
+#endif // HANDHELDDEVICE_H
